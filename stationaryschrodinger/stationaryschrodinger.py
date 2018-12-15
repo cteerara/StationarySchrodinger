1e877ad4d59a90d5de61511ece64d5f006452e40
Chayut Teeraratkul <cteerara@u.rochester.edu>
2018-12-15 10:27:19 -0500

    ss cleanup

--- stationaryschrodinger/stationaryschrodinger.py
+++ stationaryschrodinger/stationaryschrodinger.py
@@ -1,21 +1,21 @@
 # -*- coding: utf-8 -*-
-import tensorflow as tf
-tf.enable_eager_execution();
-import IO
-import tfAPI
-import EigFunc
-
-
-
-
-
-# ------------------------------------------------------
-# ------------ MAIN ------------------------------------
-# ------------------------------------------------------
-def main():
-    Pos, PotEn = IO.readPotentialEnergy("potential_energy.dat")
-    print(Pos)
-    print(PotEn)
+#import tensorflow as tf
+#tf.enable_eager_execution();
+#import IO
+#import tfAPI
+#import EigFunc
+#
+#
+#
+#
+#
+## ------------------------------------------------------
+## ------------ MAIN ------------------------------------
+## ------------------------------------------------------
+#def main():
+#    Pos, PotEn = IO.readPotentialEnergy("potential_energy.dat")
+#    print(Pos)
+#    print(PotEn)
 
 
 
--- tests/test_stationaryschrodinger.py
+++ tests/test_stationaryschrodinger.py
@@ -4,6 +4,7 @@
 """Tests for `stationaryschrodinger` package."""
 
 import pytest
+import math
 import sys
 import os
 import tensorflow as tf
@@ -16,6 +17,7 @@ from stationaryschrodinger import stationaryschrodinger
 from stationaryschrodinger import cli
 from stationaryschrodinger import tfAPI
 from stationaryschrodinger import IO
+from stationaryschrodinger import EigFunc
 
 @pytest.fixture
 def response():
@@ -47,13 +49,15 @@ def test_command_line_interface():
 #------------- TEST tfAPI.py ---------------------------------------#
 #-------------------------------------------------------------------#
 
-def test_tfAPI_compare():
+def test_compare():
   t1 = tf.constant([1],dtype=tf.float64)
   t2 = tf.constant([[1],[2]],dtype=tf.float64)
   t3 = tf.constant([[1,1],[2,2]],dtype=tf.float64)
   t4 = tf.constant([[1,1],[2,3]],dtype=tf.float64)
   # Case: dimension is not equal
   assert(not tfAPI.compare(t1,t2))
+  # Case: type mismatch
+  assert(not tfAPI.compare(t1,tf.cast(t1,tf.float32)))
   # Case: size noe equal
   assert(not tfAPI.compare(t2,t3))
   # Case: elements not equal
@@ -61,19 +65,21 @@ def test_tfAPI_compare():
   # case: IS equal
   assert(tfAPI.compare(t3,t3+1e-7)) # Check if it exceeds tol
 
-def test_tfAPI_zeroes():
-  t1 = tfAPI.zeroes([2,2])
-  t2 = tf.Variable([[0.,0.],[0.,0.]],dtype=tf.float64)
-  assert(tfAPI.compare(t1,t2))
-
 def test_tfdim():
-  t1 = tfAPI.zeroes([2,2])
+  t1 = tf.constant(0,shape=[2,2])
   tdim = tf.constant(2)
   assert(tfAPI.compare(tfAPI.tfdim(t1),tdim))
 
-#def test_tflen():
-#  t1 = tf.constant([1,2,3])
-#  assert(tfAPI.compare( tfAPI.tflen(t1),tf.reshape(tf.shape(t),[]) ))
+def test_tflen():
+  t1 = tf.constant([1,2,3])
+  assert(tfAPI.compare( tfAPI.tflen(t1),tf.constant(3)))
+
+def test_integrate():
+  x = tf.Variable([2,4,6])
+  F = tf.Variable([1,1,1])
+  Fint = tf.Variable(4)
+  assert(tfAPI.compare( Fint, tfAPI.integrate(F,F,x)))
+  
 #-------------------------------------------------------------------#
 #------------- TEST IO.py ------------------------------------------#
 #-------------------------------------------------------------------#
@@ -81,4 +87,42 @@ def test_tfdim():
 def test_IO_readPotentialEnergy():
   x = IO.readPotentialEnergy('tests/IOtest.dat')
   assert(tfAPI.compare(x[0],x[0]))
+
+#-------------------------------------------------------------------#
+#------------- TEST EigFunc.py -------------------------------------#
+#-------------------------------------------------------------------#
+
+def test_FPoly():
+  pi = 4*math.atan(1.0)
+  x = [0,pi/2,pi]
+  x = tf.constant(x,dtype=tf.float32)
+  b = tf.cast(EigFunc.FPoly(x,3),tf.float32)
+  bActual = [tf.constant([1,1,1],dtype=tf.float32),tf.constant([0,1,0],dtype=tf.float32),tf.constant([1,0,-1],dtype=tf.float32)]
+  for i in range(0,3):  
+    assert(tfAPI.compare( b[i] , bActual[i]))
+
+def test_project():
+  pi = 4*math.atan(1.0)
+  x = [0,pi/2,pi]
+  x = tf.constant(x,dtype=tf.float32)
+  b = EigFunc.FPoly(x,3)
+  F = tf.Variable([0,1,0],dtype=tf.float32)
+  FProjActual = tf.constant([0,1,0],dtype=tf.float32)
+  FProj = EigFunc.project(F,b,x)
+  assert(tfAPI.compare(FProj,FProjActual))
+
+def test_Hij():
+  pi = 4*math.atan(1.0)
+  x = [0,pi/2,pi]
+  x = tf.constant(x,dtype=tf.float32)
+  b = EigFunc.FPoly(x,3)
+  c = 1
+  V = tf.constant([0,1,0],dtype=tf.float32)
+  VProj = EigFunc.project(V,b,x)
+  Hij = EigFunc.hamil(x,b,c,VProj)
+  HijActual = tf.constant([[0,0,0],[1,c+1,1],[0,0,1]],dtype=tf.float32)
+  assert(tfAPI.compare(Hij,HijActual))
+
+
+
 
